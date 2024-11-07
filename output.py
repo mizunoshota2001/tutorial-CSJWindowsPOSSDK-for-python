5e112c98ab5b88ceeb5766960dc89ab4360fb081
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-07 22:01:18 +0900

    output.py ファイルを削除

--- output.py
+++
@@ -1,7 +0,0 @@
-import clr
-from pathlib import Path
-__path = Path(__file__).parent/"Library"/"CSJPOSLib.dll"
-clr.AddReference(str(__path))
-import com.citizen.sdk
-tmp=com.citizen.sdk.ESCPOSConst
-print({k:getattr(tmp, k) for k in dir(tmp)})

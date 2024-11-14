a61c5864be0f7c8b49291a4691d16469ae92b5e4
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-14 12:45:23 +0900

    不要なプリンタ定数の参照を削除し、コードの可読性を向上させるために整形

--- samples/2.3.8.PrinterCheck.smp.py
+++ samples/2.3.8.PrinterCheck.smp.py
@@ -17,14 +17,15 @@ if ESCPOSConst.CMP_STS_NORMAL == status:
     if ESCPOSConst.CMP_STS_PAPER_NEAREMPTY & status2 > 0:
         print("Paper is near empty")
 else:
-    if printer.ESCPOSConst.CMP_STS_COVER_OPEN & status > 0:
+    if ESCPOSConst.CMP_STS_COVER_OPEN & status > 0:
         print("Cover is open")
-    if printer.ESCPOSConst.CMP_STS_PAPER_EMPTY & status > 0:
+    if ESCPOSConst.CMP_STS_PAPER_EMPTY & status > 0:
         print("Paper is empty")
-    if printer.SearchESCPOSConst.CMP_STS_PRINTEROFF & status > 0:
+    if ESCPOSConst.CMP_STS_PRINTEROFF & status > 0:
         print("Printer is off")
 
-status3 = printer.Status(ESCPOSConst.CMP_STS_DRAWER_LEVEL_H | ESCPOSConst.CMP_STS_PAPER_ONPRESENTER)
+status3 = printer.Status(ESCPOSConst.CMP_STS_DRAWER_LEVEL_H |
+                         ESCPOSConst.CMP_STS_PAPER_ONPRESENTER)
 if ESCPOSConst.CMP_STS_DRAWER_LEVEL_H & status3 > 0:
     print("Status of pin 3 of drawer kick-out connector = H")
 if ESCPOSConst.CMP_STS_PAPER_ONPRESENTER & status3 > 0:
